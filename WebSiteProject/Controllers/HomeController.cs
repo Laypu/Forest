012098dffde12beffa1d5029a09608f554b2bc80
Yongsheng10 <54974398+Yongsheng10@users.noreply.github.com>
2020-03-18 17:47:00 +0800

    update article

--- WebSiteProject/Controllers/HomeController.cs
+++ WebSiteProject/Controllers/HomeController.cs
@@ -675,9 +675,123 @@ namespace WebSiteProject.Controllers
             return View(viewmodel);
         }
 
-        public ActionResult Article(int? langid, string ImgTitle)
+        public ActionResult ArticleSearch(int? langid, string Hashtag)
         {
-            var site_id = 3;
+            var site_id = 4;
+            if (Session["LangID"] == null)
+            {
+                var DefaultLang = System.Web.Configuration.WebConfigurationManager.AppSettings["DefaultLang"];
+                _ILangManager = serviceinstance.LangManager;
+                var alllang = _ILangManager.GetAll();
+                if (alllang != null)
+                {
+                    if (alllang.Any(v => v.Lang_Name == DefaultLang))
+                    {
+                        langid = alllang.Where(v => v.Lang_Name == DefaultLang).First().ID.Value;
+                    }
+                }
+                Session["LangID"] = langid.ToString();
+                Session.Timeout = 600;
+            }
+            else
+            {
+
+                if (langid == null)
+                {
+                    int _langid = 1;
+                    if (int.TryParse(Session["LangID"].ToString(), out _langid) == false)
+                    {
+                        langid = 1;
+                    }
+                    else { langid = _langid; }
+                }
+                else
+                {
+                    Session["LangID"] = langid.ToString();
+                }
+            }
+
+            HomeViewModel viewmodel = new HomeViewModel();
+            //讀取logo圖片
+            _IMasterPageManager.SetModel<HomeViewModel>(ref viewmodel, "P", langid.ToString(), "");
+            viewmodel.SEOScript = _IMasterPageManager.GetSEOData("", "", langid.ToString());
+            viewmodel.ADMain = _IMasterPageManager.GetADMain("P", langid.ToString(), site_id);
+            viewmodel.ADMobile = _IMasterPageManager.GetADMain("M", langid.ToString(), site_id);
+            viewmodel.TrainingSiteData = _ISiteLayoutManager.GetTrainingSiteData(Common.GetLangText("另開新視窗")).AntiXss(new string[] { "class" });
+
+            var sitemenu = _ISiteLayoutManager.PagingMain(new ViewModels.SearchModelBase()
+            {
+                Limit = 100,
+                Key = Device,
+                NowPage = 1,
+                Offset = 0,
+                Sort = "ID",
+                LangId = langid.ToString()
+            });
+            if (sitemenu.total > 0)
+            {
+                var layoutpagelist = ((List<PageLayout>)sitemenu.rows);
+                if (layoutpagelist.Any(v => v.Title == "焦點新聞"))
+                {
+                    viewmodel.PageLayoutModel1 = _IMasterPageManager.GetSiteLayout(sitemenu.rows, "焦點新聞", langid.ToString()).First();
+                }
+                else { viewmodel.PageLayoutModel1 = new HomePageLayoutModel(); }
+                if (layoutpagelist.Any(v => v.Title == "活動專區"))
+                {
+                    viewmodel.PageLayoutModel2 = _IMasterPageManager.GetSiteLayout(sitemenu.rows, "活動專區", langid.ToString()).First();
+                }
+                else { viewmodel.PageLayoutModel2 = new HomePageLayoutModel(); }
+            }
+            else
+            {
+                viewmodel.PageLayoutModel1 = new HomePageLayoutModel();
+                viewmodel.PageLayoutModel2 = new HomePageLayoutModel();
+                ViewBag.sitemenu = new List<PageLayout>();
+                ViewBag.sitemenupart = "";
+            }
+            viewmodel.BannerImage = "";
+            viewmodel.PageLayoutOP1 = _ISiteLayoutManager.GetPageLayoutOP1Edit(langid.ToString());
+            viewmodel.PageLayoutOP2 = _ISiteLayoutManager.GetPageLayoutOP2Edit(langid.ToString());
+            viewmodel.PageLayoutOP3 = _ISiteLayoutManager.GetPageLayoutOP3Edit(langid.ToString());
+            viewmodel.PageLayoutActivityModel = _ISiteLayoutManager.PageLayoutActivity(langid.ToString());
+
+            viewmodel.LinkItems = _IModelLinkManager.PagingItem("Y", new SearchModelBase()
+            {
+                LangId = this.LangID,
+                Limit = -1,
+                Sort = "Sort"
+            }).rows;
+
+            var q = from M in db.MessageItems
+                    join H in db.Message10Hash
+                    on M.ItemID equals H.MessageItem_ID
+                    where H.Message10Hash_1H == Hashtag ||
+                          H.Message10Hash_2H == Hashtag ||
+                          H.Message10Hash_3H == Hashtag ||
+                          H.Message10Hash_4H == Hashtag ||
+                          H.Message10Hash_5H == Hashtag ||
+                          H.Message10Hash_6H == Hashtag ||
+                          H.Message10Hash_7H == Hashtag ||
+                          H.Message10Hash_8H == Hashtag ||
+                          H.Message10Hash_9H == Hashtag ||
+                          H.Message10Hash_10H == Hashtag
+                    select M;
+
+            ViewBag.Hash = q.ToList();
+
+            ViewBag.Message10Hash = db.Message10Hash.ToList();
+
+            ViewBag.HashTagName = Hashtag;
+            return View(viewmodel);
+        }
+
+        public ActionResult Article(int? langid = 1, int? Aid = 37, string Hashtag = "test")
+        {
+            ViewBag.Unit = db.MessageUnitSettings.Where(p => p.MainID == 9).Select(p => new UnitPrint { isPrint = (bool)p.IsPrint, isForward = (bool)p.IsForward, isRSS = (bool)p.IsRSS, isShare = (bool)p.IsShare }).FirstOrDefault();
+            //ViewBag.PagePrt = Url.Action("Print", "Home", new { id = RecommendedTrips_ID });
+
+
+            var site_id = 4;
             if (Session["LangID"] == null)
             {
                 var DefaultLang = System.Web.Configuration.WebConfigurationManager.AppSettings["DefaultLang"];
@@ -762,9 +876,13 @@ namespace WebSiteProject.Controllers
                 Sort = "Sort"
             }).rows;
             
-            ViewBag.DesHash = db.MessageItems.Where(M => M.Title == ImgTitle).ToList();
+            ViewBag.DesHash = db.MessageItems.Where(M => M.ItemID == Aid).ToList();
+            ViewBag.MessageBanner = db.MessageBanners.Where(B => B.MessageItem_ID == Aid).First().MessageBanner_Img;
+            ViewBag.HashTagName = Hashtag;
             return View(viewmodel);
         }
+
+
 
     }
 }
